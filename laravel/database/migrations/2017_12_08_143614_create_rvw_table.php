cadf1a1c3e6501963673d7e4d5651604284c4d7b
Erika Pepe <n01201502@humbermail.ca>
2017-12-08 10:12:09 -0500

    Creates Review (RVW) Table

---
+++ laravel/database/migrations/2017_12_08_143614_create_rvw_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRvwTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rvw', function (Blueprint $table) {
+            $table->increments('rvw_id_review')->primary();
+            $table->string('rvw_st_review')->nullable();
+            $table->decimal('rvw_dc_rate', 6, 2);
+            $table->foreign('rvw_id_user')->references('usr_id_user')->on('usr');
+            $table->foreign('rvw_id_drink')->references('drk_id_drink')->on('drk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rvw');
+    }
+}
