08881219c0b80260b29a826d6a52bfeed1bf639e
Erika Pepe <n01201502@humbermail.ca>
2017-12-08 10:15:14 -0500

    Creates Wishlist (WSL) Table

---
+++ laravel/database/migrations/2017_12_08_143655_create_wsl_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWslTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wsl', function (Blueprint $table) {
+            $table->primary(['wsl_id_user', 'wsl_id_drink']);
+            $table->foreign('wsl_id_user')->references('usr_id_user')->on('usr');
+            $table->foreign('wsl_id_drink')->references('drk_id_drink')->on('drk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wsl');
+    }
+}
