4ce055a64dd978feac34035d67400376ecd503e8
chezaresarmiento <sarmiento_cesar@outlook.com>
2018-01-15 13:55:28 -0500

    Display Category by store and drink page v2

---
+++ laravel/app/Http/Controllers/DrinkCategoryGridFullController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hamcrest\Arrays\IsArrayContainingInAnyOrder;
+use Illuminate\Http\Request;
+
+class DrinkCategoryGridFullController extends Controller
+{
+    //
+
+
+    public function showSubCategories($id, $category, $page = null)
+    {
+        //$id=$request['id'];
+        //$category=$request['category'];
+       echo  $categorybeforecurl=$category;
+        $category_secondary = Array();
+        //$page=$request['page'];
+        if ($page == null) {
+            $page = 1;
+        }
+        $greatjason = Array();
+
+        $url2 = 'http://lcboapi.com/products?per_page=100&store_id=' . $id . '&q=' . $category . '&page=' . $page;
+        $curl2 = curl_init($url2);
+        curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
+        $json2 = curl_exec($curl2);
+        $obj2 = json_decode($json2, true);
+
+        $total_pages = $obj2["pager"]["total_pages"];
+        $total_record_count = $obj2["pager"]["total_record_count"];
+
+
+        echo "The current Category is : " . $category . "<br>";
+        echo "Total of record is : " . $total_record_count . "<br>";
+        echo "Total pages : " . $total_pages . "<br>";
+        echo "Current store id is : " . $id . "<br>";
+
+        echo $categoryaftercurl=$obj2["result"][0]["primary_category"];
+
+
+        foreach ($obj2["result"] as $drink) {
+
+
+                if ($drink['primary_category'] == $category & !in_array($drink['secondary_category'], $category_secondary)) {
+                    array_push($category_secondary, $drink['secondary_category']);
+                };
+            };
+
+
+        foreach ($obj2["result"] as $drink) {
+            if ($drink['primary_category'] == $category) {
+                $product = Array();
+                $product["id"] = $drink["id"];
+                $product["name"] = ($drink["name"] == null ? "" : $drink["name"]);
+                $product["primary_category"] = $drink["primary_category"];
+                $product["secondary_category"] = $drink["secondary_category"];
+                $product["origin"] = $drink["origin"];
+                $product["package"] = $drink["package"];
+                $product["package_unit_type"] = $drink["package_unit_type"];
+                $product["package_unit_volume_in_milliliters"] = $drink["package_unit_volume_in_milliliters"];
+                $product["total_package_units"] = $drink["total_package_units"];
+                $product["alcohol_content"] = $drink["alcohol_content"];
+                $product["description"] = ($drink["tags"] == null ? "ND" : $drink["tags"]);
+                $product["regular_price_in_cents"] = "$" . $drink["regular_price_in_cents"] * 100;
+                $product["image_thumb_url"] = ($drink["image_thumb_url"] == null ? "" : $drink["image_thumb_url"]);
+                $product["image_url"] = $drink["image_url"];
+
+                array_push($greatjason, $product);
+            };
+        };
+        array_unique($category_secondary);
+
+
+//var_dump($category_secondary);
+        //var_dump($greatjason);
+        echo $current_page = $obj2["pager"]["current_page"];
+        "Current Page is : " . $current_page . "<br>";
+
+        $data = array('drinks' => $greatjason, 'size' => count($greatjason), 'category' => $category, 'total_pages' => $total_pages, 'current_page' => $current_page,'category_before_curl'=>$categorybeforecurl,'category_after_curl'=>$categorybeforecurl);
+
+        return \View::make('drink-category-grid-full', ['id' => $id, 'category' => $category, 'page' => $page])->with(compact('data'));
+
+    }
+
+
+
+
+
+
+
+    public function showCategories()
+    {
+        $url2 = 'http://lcboapi.com/products?';
+        $curl2 = curl_init($url2);
+        curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
+        $json2 = curl_exec($curl2);
+        $obj2 = json_decode($json2, true);
+
+        $total_pages = $obj2["pager"]["total_pages"];
+        //$total_record_count = $obj2["pager"]["total_record_count"];
+
+        $data=$total_pages;
+
+        return \View::make('test',['pages'=>$total_pages])->with(compact('data'));
+
+    }
+
+}
+
+
+?>
---
+++ laravel/app/Http/Controllers/DrinkPageController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DrinkPageController extends Controller
+{
+    //
+    public function showProduct($idproduct){
+
+        $data=Array();
+
+        $url ='http://lcboapi.com/products/'.$idproduct;
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $json = curl_exec($curl);
+        $obj = json_decode($json,true);
+
+        $data['id']=($obj["result"]["id"]==null?"ND":$obj["result"]["id"]);
+        $data['name']=($obj["result"]["name"]==null?"ND":$obj["result"]["name"]);
+        $data['tags']=($obj["result"]["tags"]==null?"ND":$obj["result"]["tags"]);
+        $data['price_in_cents']=($obj["result"]["price_in_cents"]==null?"ND":"$ ".($obj["result"]["price_in_cents"]/100)." CAD");
+        $data['regular_price_in_cents']=($obj["result"]["regular_price_in_cents"]==null?"ND":"$ ".($obj["result"]["regular_price_in_cents"]/100)." CAD");
+        $data['limited_time_offer_savings_in_cents']=($obj["result"]["limited_time_offer_savings_in_cents"]==null?"ND":"$ ".($obj["result"]["limited_time_offer_savings_in_cents"]/100)." CAD");
+        $data['regular_price_in_cents']=($obj["result"]["regular_price_in_cents"]==null?"ND":"$ ".($obj["result"]["regular_price_in_cents"]/100)." CAD");
+        $data['limited_time_offer_ends_on']=($obj["result"]["limited_time_offer_ends_on"]==null?"ND":$obj["result"]["limited_time_offer_ends_on"]);
+        $data['stock_type']=($obj["result"]["stock_type"]==null?"ND":$obj["result"]["stock_type"]);
+        $data['primary_category']=($obj["result"]["primary_category"]==null?"ND":$obj["result"]["primary_category"]);
+        $data['secondary_category']=($obj["result"]["secondary_category"]==null?"ND":$obj["result"]["secondary_category"]);
+        $data['origin']=($obj["result"]["origin"]==null?"ND":$obj["result"]["origin"]);
+        $data['package']=($obj["result"]["package"]==null?"ND":$obj["result"]["package"]);
+        $data['package_unit_type']=($obj["result"]["package_unit_type"]==null?"ND":$obj["result"]["package_unit_type"]);
+        $data['package_unit_volume_in_milliliters']=($obj["result"]["package_unit_volume_in_milliliters"]==null?"ND":$obj["result"]["package_unit_volume_in_milliliters"]);
+        $data['total_package_units']=($obj["result"]["total_package_units"]==null?"ND":$obj["result"]["total_package_units"]);
+        $data['volume_in_milliliters']=($obj["result"]["volume_in_milliliters"]==null?"ND":$obj["result"]["volume_in_milliliters"]);
+        $data['alcohol_content']=($obj["result"]["alcohol_content"]==null?"ND":($obj["result"]["alcohol_content"]/100)."%");
+        $data['producer_name']=($obj["result"]["producer_name"]==null?"ND":$obj["result"]["producer_name"]);
+        $data['has_value_added_promotion']=($obj["result"]["has_value_added_promotion"]==null?"ND":$obj["result"]["has_value_added_promotion"]);
+        $data['value_added_promotion_description']=($obj["result"]["value_added_promotion_description"]==null?"ND":$obj["result"]["value_added_promotion_description"]);
+        $data['description']=($obj["result"]["description"]==null?"ND":$obj["result"]["description"]);
+        $data['tasting_note']=($obj["result"]["tasting_note"]==null?"ND":$obj["result"]["tasting_note"]);
+        $data['image_thumb_url']=($obj["result"]["image_thumb_url"]==null?"ND":$obj["result"]["image_thumb_url"]);
+        $data['image_url']=($obj["result"]["image_url"]==null?"ND":$obj["result"]["image_url"]);
+        $data['varietal']=($obj["result"]["varietal"]==null?"ND":$obj["result"]["varietal"]);
+        $data['style']=($obj["result"]["style"]==null?"ND":$obj["result"]["style"]);
+        $data['tertiary_category']=($obj["result"]["tertiary_category"]==null?"ND":$obj["result"]["tertiary_category"]);
+        $data['clearance_sale_savings_in_cents']=($obj["result"]["clearance_sale_savings_in_cents"]==null?"ND":$obj["result"]["clearance_sale_savings_in_cents"]);
+        $data['clearance_sale_savings_in_cents']=($obj["result"]["clearance_sale_savings_in_cents"]==null?"ND":"$ ".($obj["result"]["clearance_sale_savings_in_cents"]*100)." CAD");
+        $data['has_clearance_sale']=($obj["result"]["has_clearance_sale"]==null?"ND":$obj["result"]["has_clearance_sale"]);
+
+
+        return \View::make('drink-page',['idproduct'=>$idproduct])->with(compact('data'));
+    }
+
+}
