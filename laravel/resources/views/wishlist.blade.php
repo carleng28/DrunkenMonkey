8fe4c738a5ff3d5faafea12179e8ebb0c6d5601d
Erika Pepe <n01201502@humbermail.ca>
2018-01-21 01:37:02 -0500

    Search autocomplete - nearLocation

--- laravel/resources/views/wishlist.blade.php
+++ laravel/resources/views/wishlist.blade.php
@@ -120,9 +120,6 @@
 								</a>
 							</li>
 							<li class="active"><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">4</a></li>
 							<li>
 								<a href="#" aria-label="Next">
 									<span aria-hidden="true"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
